3e72a6ed2c9579337032baeb9e5f15d05c0892ac
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2022-09-22 11:26:46 +0300

    FirstCommit(turo) and wrong branch - sorry about that

---
+++ app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/RecipeLibraryActivity.java
@@ -0,0 +1,14 @@
+package fi.metropolia.javacrew.wellnesswizardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipeLibraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_library);
+    }
+}
