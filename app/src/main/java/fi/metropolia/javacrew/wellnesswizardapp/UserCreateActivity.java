590778b3ec954d344fee5e304069d9ee68942d75
RauliYoung <tristan.ellenberg@gmail.com>
2022-09-26 10:55:13 +0300

    changed name

--- app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/UserCreateActivity.java
+++ app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/UserCreateActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class User_create_activity extends AppCompatActivity {
+public class UserCreateActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
