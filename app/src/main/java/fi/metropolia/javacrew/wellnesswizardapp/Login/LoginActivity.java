48a1c652a64677cb05228ebd7bd33a931a387c71
Mashadeve <maenpaa.nk@gmail.com>
2022-09-26 23:19:11 +0300

    Loginactivity luokan luominen

--- app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/Login/LoginActivity.java
+++ app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/Login/LoginActivity.java
@@ -2,19 +2,13 @@ package fi.metropolia.javacrew.wellnesswizardapp.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-
-import java.util.Locale;
+import android.widget.TextView;
 
 import fi.metropolia.javacrew.wellnesswizardapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-
-    EditText username;
-    Button enterButton;
 
 
     @Override
@@ -24,23 +18,11 @@ public class LoginActivity extends AppCompatActivity {
 
         //Luodaan username muuttuja PlainText componentille,
         //ja luodaan yhteys componentiin id:n avulla.
-        username = findViewById(R.id.editTextTextUsername);
+        EditText username = findViewById(R.id.editTextTextUsername);
         username.setText("");
-
-        //Luodaan yhteys Enter-nappulaan
-        enterButton = findViewById(R.id.enterButton);
-
-
     }
 
-    public void enterTestActivity(){
-        enterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                username.getText().toString().toLowerCase(Locale.ROOT);
 
-            }
-        });
-    }
+
 
 }
