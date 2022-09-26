44d7d770be9d8a6635bf63a54fa0dc6cd3334601
Mashadeve <maenpaa.nk@gmail.com>
2022-09-26 15:45:10 +0300

    login layout fiksailua

--- app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/Login/Login.java
+++ app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/Login/Login.java
@@ -1,4 +1,16 @@
 package fi.metropolia.javacrew.wellnesswizardapp.Login;
 
-public class Login {
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+
+public class Login{
+    private String username;
+    private Button enterButton;
+    private Button signUpButton;
+
+
+
+
 }
