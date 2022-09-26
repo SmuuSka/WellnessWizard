a641d6ca66bd4c8a7906c05cedae100083565ee4
RauliYoung <tristan.ellenberg@gmail.com>
2022-09-26 10:52:16 +0300

    user create activity added

---
+++ app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/User_create_activity.java
@@ -0,0 +1,14 @@
+package fi.metropolia.javacrew.wellnesswizardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User_create_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_create);
+    }
+}
