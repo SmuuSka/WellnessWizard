2a645cce9b3bafb05c8632e97668ea0e91456bfa
RauliYoung <tristan.ellenberg@gmail.com>
2022-09-26 22:36:58 +0300

    implemented object save functionality

--- app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/UserCreateActivity.java
+++ app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/UserCreateActivity.java
@@ -1,14 +1,21 @@
 package fi.metropolia.javacrew.wellnesswizardapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.gson.Gson;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class UserCreateActivity extends AppCompatActivity {
@@ -17,7 +24,11 @@ public class UserCreateActivity extends AppCompatActivity {
 
     private Henkilo person;
 
-    EditText name, gender, age, weight, height;
+    EditText name;
+    EditText gender;
+    EditText age;
+    EditText weight;
+    EditText height;
     private String personName, personGender;
     private int personAge, personHeight;
     private double personWeight;
@@ -29,19 +40,12 @@ public class UserCreateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_create);
         //starts
         sharedPreferences = getSharedPreferences("Henkilo", Activity.MODE_PRIVATE);
-
+        //Parameters from user inputs.
         name = (EditText) findViewById(R.id.editTextTextPersonName);
         gender = (EditText) findViewById(R.id.editTextTextPersonNameGender);
         age = (EditText) findViewById(R.id.editTextNumberAge);
         height = (EditText) findViewById(R.id.editTextNumberHeight);
         weight = (EditText) findViewById(R.id.editTextNumberDecimalWeight);
-
-        personName = name.toString();
-        personGender = gender.toString();
-        personAge = Integer.parseInt(age.toString());
-        personHeight = Integer.parseInt(height.toString());
-        personWeight = Double.parseDouble(weight.toString());
-
 
 
         Button send = findViewById(R.id.buttonSendData);
@@ -50,20 +54,43 @@ public class UserCreateActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //save data and move to next activity.
-                Henkilo uusiKayttaja = new Henkilo(personName, personAge, personHeight, personWeight);
-                System.out.println(uusiKayttaja);
+
+                personName = name.getText().toString();
+                personGender = gender.getText().toString();
+                personAge = Integer.parseInt(age.getText().toString());
+                personHeight = Integer.parseInt(height.getText().toString());
+                personWeight = Double.parseDouble(weight.getText().toString());
+
+                Henkilo uusiKayttaja = new Henkilo(personName, personAge, personHeight, personWeight, personGender);
+                System.out.println(uusiKayttaja.toString());
                 System.out.println(personName + personAge + personHeight + personWeight + personGender);
+                saveData(uusiKayttaja);
+                Henkilo ladattu = loadData();
+                System.out.println(ladattu + " ladattu 60 sekunnissa");
             }
         });
+
         //ends
     }
 
-/*    private void saveData(String key, Henkilo henkilo) {
+
+    private void saveData(Henkilo henkilo) {
         //Might be for another acivity.
         SharedPreferences prefPut = getSharedPreferences("Henkilo", Activity.MODE_PRIVATE);
         SharedPreferences.Editor prefEditor = prefPut.edit();
-        prefEditor.putString(key, henkilo); //put object here somehow putString is temprary.
+        Gson gson = new Gson();
+        String json = gson.toJson(henkilo);
+        prefEditor.putString("Henkilo", json);
+        System.out.println(json);
         prefEditor.commit();
+    }
 
-    }*/
+    private Henkilo loadData() {
+        SharedPreferences prefPut = getSharedPreferences("Henkilo", Activity.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = prefPut.getString("Henkilo", null);
+        Henkilo person = gson.fromJson(json, Henkilo.class);
+        System.out.println(json + " load data json");
+        return person;
+    }
 }
