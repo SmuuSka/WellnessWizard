841de7ba536ac42c76d1c5fc71af37bb22cbb41d
Mashadeve <maenpaa.nk@gmail.com>
2022-09-28 21:16:38 +0300

    clean up

--- app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/Login/Login.java
+++ app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/Login/Login.java
@@ -6,9 +6,7 @@ import android.widget.Button;
 import android.view.View.OnClickListener;
 
 public class Login{
-    private String username;
-    private Button enterButton;
-    private Button signUpButton;
+
 
 
 
