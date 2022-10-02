cdddd6a8f6b588b57a98074415cb253a961107bf
Mashadeve <maenpaa.nk@gmail.com>
2022-10-02 13:08:20 +0300

    enter buttonin luominen ja fiksailua

--- app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/Login/Login.java
+++ app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/Login/Login.java
@@ -2,11 +2,11 @@ package fi.metropolia.javacrew.wellnesswizardapp.Login;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 
 public class Login{
-
 
 
 
--- app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/Login/LoginActivity.java
+++ app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/Login/LoginActivity.java
@@ -2,13 +2,17 @@ package fi.metropolia.javacrew.wellnesswizardapp.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import fi.metropolia.javacrew.wellnesswizardapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+    EditText username;
+    Button enterButton = findViewById(R.id.enterButton);
 
 
     @Override
@@ -18,11 +22,20 @@ public class LoginActivity extends AppCompatActivity {
 
         //Luodaan username muuttuja PlainText componentille,
         //ja luodaan yhteys componentiin id:n avulla.
-        EditText username = findViewById(R.id.editTextTextUsername);
+        username = findViewById(R.id.editTextTextUsername);
         username.setText("");
+
+
+
     }
 
-
-
+    public void enterTestActivity(){
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+    }
 
 }
