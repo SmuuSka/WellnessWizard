10653b3103a7c0256f5d5178f2c6a4ba26067e01
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2022-10-03 14:33:57 +0300

    RecipeLibrary and training library updates.(turo)
    Not even near ready.

--- app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/recipe/RecipeInfoHolder.java
+++ app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/recipe/RecipeInfoHolder.java
@@ -18,8 +18,33 @@ public class RecipeInfoHolder {
     }
 
     private  RecipeInfoHolder(){
+
+        recipes.add(new Recipe("Aamupuuro", "jotain jotain jotain", 3000));
         recipes.add(new Recipe("Bolognese", "200g jauheliha \n 1 sipuli \n 1 valkosipuli \n 1dl valkoviini \n 1tlk tomaattimurska \n \n bvlvbhfjlkavbhfjavbhfsalvbhsfalvbhafslbvhsfalbvhsfajlvbshfajlbvashjlvbashjlvbhsajlbvhajl", 345));
-        recipes.add(new Recipe("Lihapullat", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Curry Kanaa", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Donitsi", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Eggs Benedict", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Fetapiiras", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Gratinoitu kukkakaali", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Hernekeitto", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Inkiväärikalaa", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Juustokeitto", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Kalakeitto", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Lihapullakastike", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Melanzane", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Nokkosletut", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Omeletti", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Paistetut silakat", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Quiche Lorainne", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Ragu", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Salmiakkijäätelö", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Taatelikakku", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Uunilohi", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Vispipuuro", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Xante päärynät", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Yrttimarinoitua kanaa", "jotain jotain jotain", 3000));
+        recipes.add(new Recipe("Zuccini Fritters", "jotain jotain jotain", 3000));
+
 
     }
     public List<Recipe> getResipes(){
