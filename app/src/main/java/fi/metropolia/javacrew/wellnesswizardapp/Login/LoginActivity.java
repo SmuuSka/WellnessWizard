88b1a572a0913569aa36fa478400a46a78b5066b
Mashadeve <maenpaa.nk@gmail.com>
2022-10-05 20:22:03 +0300

    Aakkoset vaihdettu A-Z, numerot ja merkit poistettu edittextistä.

--- app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/Login/LoginActivity.java
+++ app/src/main/java/fi/metropolia/javacrew/wellnesswizardapp/Login/LoginActivity.java
@@ -6,7 +6,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.text.InputType;
+import android.text.Spanned;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,35 +26,43 @@ public class LoginActivity extends AppCompatActivity {
 
     public static final String EXTRA_BERBA = "username.EXTRA_BERBA";
 
-    //Luodaan username ja enterButton muuttujat
     private EditText username;
     private Button enterButton;
-
-    //väliaikainen ohitus nappula
-    private Button bypassButton;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
+        username = findViewById(R.id.editTextTextUsername);
+
         Henkilo loaded = loadData();
         if (loaded != null){
             Henkilo.setInstance(loaded);
-            System.out.println("Tämä tulee iffistä");
             Intent bypassIntent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(bypassIntent);
         } else {
-            username = findViewById(R.id.editTextTextUsername);
             username.setText("");
-            System.out.println("Tämä tulee elsestä");
         }
 
         username.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
 
         enterButton = findViewById(R.id.enterButton);
+
+
+        username.setFilters(new InputFilter[]{
+                new InputFilter() {
+                    public CharSequence filter(CharSequence charSequence, int start, int end, Spanned dst, int dstart, int dend) {
+                        if (charSequence.equals("")){
+                            return charSequence;
+                        }
+                        if (charSequence.toString().matches("[a-zA-Z]+")){
+                            return  charSequence;
+                        }
+                        return username.getText().toString();
+                    }
+                }
+        });
 
         enterButton();
 
@@ -70,7 +80,6 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String name = username.getText().toString();
-
                 Intent enterIntent = new Intent(LoginActivity.this, UserCreateActivity.class);
                 enterIntent.putExtra(EXTRA_BERBA, name);
                 startActivity(enterIntent);
